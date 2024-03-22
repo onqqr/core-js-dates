173847d44672cde0c56b3314e7748b72af57af05
onqqr <dnbyaniss@gmail.com>
2024-03-22 10:51:04 +0300

    feat: solved getNextFriday

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -74,8 +74,12 @@ function getDayName(date) {
  * Date('2024-02-13T00:00:00Z') => Date('2024-02-16T00:00:00Z')
  * Date('2024-02-16T00:00:00Z') => Date('2024-02-23T00:00:00Z')
  */
-function getNextFriday(/* date */) {
-  throw new Error('Not implemented');
+function getNextFriday(date) {
+  const day = date.getDay();
+  const nextFriday = (5 + 7 - day) % 7 || 7;
+  const newFriday = new Date(date);
+  newFriday.setDate(date.getDate() + nextFriday);
+  return newFriday;
 }
 
 /**
